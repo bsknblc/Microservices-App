aa0ad95278b205bd3f8f6470f7ad26492318490c
Başak Balcı <basakbalci@Basaks-MacBook-Pro.local>
2026-03-03 16:41:41 +0100

    circuit breaker added

--- inventory-service/src/main/java/com/bsknblc/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/bsknblc/inventory_service/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.bsknblc.inventory_service.dto.InventoryResponse;
 import com.bsknblc.inventory_service.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j //for logging
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows //don't use this in production, it will consume the expection
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000); //simulates network delay
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
